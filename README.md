1247c36a924821767bf02e3ee05c3f43051bd347
Mati Halperin <matias.halperin@gmail.com>
2017-03-31 17:53:26 -0300

    Initial bringup

---
+++ README.md
@@ -0,0 +1,99 @@
+# Git Management Tool (gmt)
+
+A library to clone multiple repos simpler
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+$ sudo apt-get install git-core
+```
+
+### Installing
+
+A step by step series of examples that tell you have to get it running
+
+Clone this repo
+
+```
+$ git clone https://github.com/MatiHalperin/gmt.git
+```
+
+And add it to the path by opening .bashrc
+
+```
+$ gedit .bashrc
+```
+
+And adding at the end of the file
+
+```
+source [PATH TO THE CLONED REPO]/gmt.sh
+```
+
+Where [PATH TO THE CLONED REPO] is the location of the folder cloned in the first step
+
+## Commands available
+
+Now that you have gmt installed, the following commands are available from any terminal
+
+### init
+
+You need to specify the URL of the parent page (where all the repos are hosted), the desired branch and the local file with all the repos
+
+```
+$ gmt init [URL] [BRANCH] [FILE]
+```
+
+### change-values
+
+You need to specify the new URL and branch
+
+```
+$ gmt change-values [URL] [BRANCH]
+```
+
+### change-source
+
+You need to specify the new file with the repos
+
+```
+$ gmt change-source [FILE]
+```
+
+### clone
+
+You don't need to specify anything. It will clone all the repos that aren't cloned already
+
+```
+$ gmt clone
+```
+
+### check
+
+It will verify if all the repos are cloned, and tell you the ones that aren't
+
+```
+$ gmt check
+```
+
+### sync
+
+It will pull all the new commits from the repos and clone the missing ones, if any
+
+```
+$ gmt sync
+```
+
+### reset
+
+It will delete the config files containing the URL, branch and repos (the file with the repos, not the repos itself)
+
+```
+$ gmt reset
+```
---
+++ gmt.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+function DownloadFinished()
+{
+    while read -r line || [[ -n $line ]];
+    do
+        FOLDER=$(echo "$line" | cut -d'"' -f 2)
+
+        if ! [ -d "$FOLDER" ];
+        then
+            echo "false"
+            return
+        fi
+    done < "$1"
+
+    echo "true"
+}
+
+function gmt()
+{
+    if [[ "$1" == "init" ]]; then
+
+        rm -rf .gmtconfig-values .gmtconfig-source
+        echo "$2;$3" > .gmtconfig-values
+        cp "$4" .gmtconfig-source
+
+    elif [[ "$1" == "change-values" ]]; then
+
+        rm -rf .gmtconfig-values
+        echo "$2;$3" > .gmtconfig-values
+
+    elif [[ "$1" == "change-source" ]]; then
+
+        rm -rf .gmtconfig-source
+        cp "$2" .gmtconfig-source
+
+    elif [[ "$1" == "clone" ]]; then
+
+        while [ "$(DownloadFinished .gmtconfig-source)" = "false" ];
+        do
+            while read -r line || [[ -n $line ]];
+            do
+                URL=$(echo "$line" | cut -d';' -f 1)
+                BRANCH=$(echo "$line" | cut -d';' -f 2)
+            done < .gmtconfig-values
+
+            while read -r line || [[ -n $line ]];
+            do
+                FOLDER=$(echo "$line" | cut -d'"' -f 4)
+                DESTINATION=$(echo "$line" | cut -d'"' -f 2)
+
+                if ! [ -d "$DESTINATION" ]
+                then
+                    git clone -b "$BRANCH" "$URL/$FOLDER" "$DESTINATION"
+                fi
+            done < .gmtconfig-source
+        done
+
+    elif [[ "$1" == "check" ]]; then
+
+        echo
+
+        while read -r line || [[ -n $line ]];
+        do
+            FOLDER=$(echo "$line" | cut -d'"' -f 2)
+
+            if ! [ -d "$FOLDER" ];
+            then
+                echo El repositorio "$FOLDER" no existe
+            fi
+        done < .gmtconfig-source
+
+        echo
+
+    elif [[ "$1" == "sync" ]]; then
+
+        while read -r line || [[ -n $line ]];
+        do
+            DESTINATION=$(echo "$line" | cut -d'"' -f 2)
+
+            cd $DESTINATION
+            git pull
+            cd -
+            echo
+        done < .gmtconfig-source
+
+    elif [[ "$1" == "reset" ]]; then
+
+        rm -rf .gmtconfig-source .gmtconfig-values
+
+    fi
+
+    for ARGUMENT in "$@"
+    do
+        if [[ "$ARGUMENT" == "--shutdown" ]]
+        then
+            systemctl poweroff
+        fi
+    done
+}
+
